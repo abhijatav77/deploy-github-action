2d8c0358239169189add09afb0287fb1911d1eef
Abhishek Kumar <abhishekdatapur@gmail.com>
2026-03-15 10:27:26 +0530

    changes

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const PORT = process.env.PORT ?? 8080
 
 app.get("/", (req, res)=>{
     res.status(200).json({
-        msg: "Hello from the server"
+        msg: "Hello from the v1 server"
     })
 })
 
-app.listen(PORT, ()=>{
+app.listen(PORT,'0.0.0.0', ()=>{
     console.log(`Server is up and running on PORT ${PORT}`)
 })
